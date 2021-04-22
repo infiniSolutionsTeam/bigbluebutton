b3727bdefe2b97a5e1882b010fc050724b76fceb
Sachitha Hirushan <shpsachitha@gmail.com>
2021-04-22 21:26:09 +0530

    removed sudo

--- bigbluebutton-html5/dev.sh
+++ bigbluebutton-html5/dev.sh
@@ -1,5 +1,5 @@
 git pull origin dev-chata
 meteor build --server-only /home/ubuntu/dev/bigbluebutton/bigbluebutton-html5/meteorbundle
-sudo tar -xzvf /home/ubuntu/dev/bigbluebutton/bigbluebutton-html5/meteorbundle/*.tar.gz -C /usr/share/meteor
-sudo systemctl stop bbb-html5
-sudo systemctl start bbb-html5
+tar -xzvf /home/ubuntu/dev/bigbluebutton/bigbluebutton-html5/meteorbundle/*.tar.gz -C /usr/share/meteor
+systemctl stop bbb-html5
+systemctl start bbb-html5
