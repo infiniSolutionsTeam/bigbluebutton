8fe471c4e257ec74832f4cacd321d9b28ace5ea3
Sachitha Hirushan <shpsachitha@gmail.com>
2021-06-06 19:47:32 +0530

    custom icon for actions bar

--- bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
@@ -87,7 +87,13 @@ class ActionsBar extends PureComponent {
               size="lg"
               btnnew
               btnMobileOnly
-              customIcon={<p>icon here</p>}
+              customIcon={
+                <svg xmlns="http://www.w3.org/2000/svg" width="27" height="7" viewBox="0 0 27 7">
+                  <circle id="Ellipse_37" data-name="Ellipse 37" cx="3.5" cy="3.5" r="3.5" fill="#f2f2f2"/>
+                  <circle id="Ellipse_38" data-name="Ellipse 38" cx="3.5" cy="3.5" r="3.5" transform="translate(10)" fill="#f2f2f2"/>
+                  <circle id="Ellipse_39" data-name="Ellipse 39" cx="3.5" cy="3.5" r="3.5" transform="translate(20)" fill="#f2f2f2"/>
+                </svg>
+              }
               hideLabel
             />
         {enableVideo
@@ -115,7 +121,14 @@ class ActionsBar extends PureComponent {
               className={cx(currentUser.emoji === 'raiseHand' || styles.btn)}
               size="lg"
               btnnew
-              customIcon={<p>icon here</p>}
+              customIcon={
+                <svg xmlns="http://www.w3.org/2000/svg" width="24.883" height="27.426" viewBox="0 0 24.883 27.426">
+                  <g id="Icon_feather-home" data-name="Icon feather-home" transform="translate(-3.5 -2)">
+                    <path id="Path_28" data-name="Path 28" d="M4.5,11.9,15.941,3l11.441,8.9V25.883a2.543,2.543,0,0,1-2.543,2.543H7.043A2.543,2.543,0,0,1,4.5,25.883Z" fill="none" stroke="#f2f2f2" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                    <path id="Path_29" data-name="Path 29" d="M13.5,30.713V18h7.628V30.713" transform="translate(-1.372 -2.287)" fill="none" stroke="#f2f2f2" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                  </g>
+                </svg>
+              }
               hideLabel
               btnMobileOnly
               
