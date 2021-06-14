8325b210849dfa73572a97a62f7a3c4dbac68555
Sachitha Hirushan <shpsachitha@gmail.com>
2021-06-14 22:04:27 +0530

    raise hand color default

--- bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
+++ bigbluebutton-html5/imports/ui/components/actions-bar/component.jsx
@@ -127,7 +127,7 @@ class ActionsBar extends PureComponent {
                 }`,
               })}
               accessKey={shortcuts.raisehand}
-              color={currentUser.emoji === 'raiseHand' ? 'primary' : 'default'}
+              color={'default'}
               data-test={currentUser.emoji === 'raiseHand' ? 'lowerHandLabel' : 'raiseHandLabel'}
               ghost={currentUser.emoji !== 'raiseHand'}
               className={cx(currentUser.emoji === 'raiseHand' || styles.btn)}
