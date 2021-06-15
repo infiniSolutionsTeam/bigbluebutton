efbd5656d863345a5e1300bcf8f895a394f614de
Sachitha Hirushan <shpsachitha@gmail.com>
2021-06-15 16:51:09 +0530

    message send button and input wip

--- bigbluebutton-html5/imports/ui/components/chat/message-form/component.jsx
+++ bigbluebutton-html5/imports/ui/components/chat/message-form/component.jsx
@@ -278,6 +278,7 @@ class MessageForm extends PureComponent {
         className={cx(className, styles.form)}
         onSubmit={this.handleSubmit}
       >
+        <TypingIndicatorContainer {...{ error }} />
         <div className={styles.wrapper}>
           <TextareaAutosize
             className={styles.input}
@@ -304,12 +305,16 @@ class MessageForm extends PureComponent {
             disabled={disabled || partnerIsLoggedOut}
             label={intl.formatMessage(messages.submitLabel)}
             color="default"
-            icon="send"
+            customIcon={
+              <svg xmlns="http://www.w3.org/2000/svg" width="31.5" height="27" viewBox="0 0 31.5 27">
+                <path id="Icon_material-send" data-name="Icon material-send" d="M3.015,31.5,34.5,18,3.015,4.5,3,15l22.5,3L3,21Z" transform="translate(-3 -4.5)" fill="#292929"/>
+              </svg>
+            }
             onClick={() => {}}
             data-test="sendMessageButton"
           />
         </div>
-        <TypingIndicatorContainer {...{ error }} />
+        
       </form>
     ) : null;
   }
